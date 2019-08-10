620a4bb1b03c30f79177118b54213eb2560ee30d
Erdem <ealkan87@gmail.com>
2019-08-11 02:45:41 +0300

    remove viewer line

--- draw.sh
+++ draw.sh
@@ -14,4 +14,4 @@ calculated_height=$((height-topbottom*2))
 
 convert $filename -fill none -stroke gray -draw "stroke-dasharray 5 5 rectangle ${leftright},${topbottom} ${calculated_with},${calculated_height}" $filenamewithoutextension"_stroke".png
 convert $filenamewithoutextension"_stroke".png scissors.png -geometry -6+50 -composite -format jpg -quality 100 $filenamewithoutextension"_final".jpg
-xviewer $filenamewithoutextension"_final".jpg &
+
